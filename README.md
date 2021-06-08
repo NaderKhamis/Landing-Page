89c908634da1f46a1dee5f7a937d9db7ebc29f8e
Nader Khamis <nadersob@gmail.com>
2021-06-08 22:25:21 +0300

    Notes Handeld

---
+++ README.md
@@ -0,0 +1,11 @@
+Project 2 : Landing Page.
+
+This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+
+Technologies :
+HTML.
+CSS.
+JavaScript.
+
+Designed By :
+student- Nader Khamis.
--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 let main = document.querySelector('main'),
     header = document.querySelector('.page__header'),
     nav = document.querySelector('#navbar__list'),
+    section,
     genDiv = document.querySelector('#generate'),
     secNum = document.querySelector('#sec-num'),
     genBtn = document.getElementById('gen-btn'),
@@ -17,14 +18,14 @@ genBtn.addEventListener('click', () => {
         let fragment = document.createDocumentFragment();
         for (let i = 1; i <= secNum.value; i++) {
             // CREATE ELEMENTS
-            var item = document.createElement('li'),
+            section = document.createElement('section');
+            let item = document.createElement('li'),
                 link = document.createElement('a'),
-                section = document.createElement('section'),
                 div = document.createElement('div'),
                 h2 = document.createElement('h2'),
                 p1 = document.createElement('p'),
                 p2 = document.createElement('p'),
-                ele = [h2, p1, p2];
+                content = [h2, p1, p2];
             // ELEMENTS CONTENT
             link.textContent = h2.textContent = `Section ${i}`;
             p1.textContent = 'A paragraph (from the Ancient Greek , , "to write beside") is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.';
@@ -32,20 +33,22 @@ genBtn.addEventListener('click', () => {
             // APPEND ELEMENTS
             item.appendChild(link);
             nav.appendChild(item);
-            for (const iterator of ele) {
-                div.appendChild(iterator);
-            }
+            content.forEach(ele => {
+                div.appendChild(ele);
+            })
             section.appendChild(div);
             fragment.appendChild(section);
             // ELEMENTS ATTRIBUTES
-            item.setAttribute('data-nav', `section${i}`);
+            link.setAttribute('data-nav', `section${i}`);
             link.className = 'menu__link';
-            link.setAttribute('href', `#section${i}`);
+            // link.setAttribute('href', `#section${i}`);
             section.setAttribute('id', `section${i}`);
-            section.setAttribute('data-nav', `section ${i}`);
+            // section.setAttribute('data-nav', `section${i}`);
             div.className = 'landing__container';
         }
+        // APPEND-FRAGMENT-TO-THE-MAIN
         main.appendChild(fragment);
+        // ADD-ACTIVE-CLASS-TO-VIEWABLE-SECTION
         window.addEventListener('scroll', () => {
             section.parentElement.querySelectorAll('section').forEach(ele => {
                 if (scrollY >= ele.offsetTop - 115) {
@@ -88,6 +91,18 @@ window.addEventListener('scroll', function() {
 
 upBtn.addEventListener('click', () => {
     scrollTo(0, 0);
+})
+
+// SCROLL-TO-SECTION-WHEN-LI-CLICKED
+function scrollToElement(event) {
+    if (event.target.nodeName === 'A') {
+        const sectionId = event.target.getAttribute('data-nav');
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+}
+nav.addEventListener('click', function(event) {
+    scrollToElement(event)
 })
 
 // END-SCROLL-TO
